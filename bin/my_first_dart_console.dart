58cd28b2aa86bc12a07c8bc47cde0e2aef14b37d
Rikky Putra <rikky.putra14@gmail.com>
2020-12-28 15:02:36 +0700

    part 2 done about Percabangan IF dan Swith

--- bin/my_first_dart_console.dart
+++ bin/my_first_dart_console.dart
@@ -2,7 +2,28 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  String input = stdin.readLineSync();
-  double number = double.tryParse(input);
-  print(number + 10);
+  int number = int.tryParse(stdin.readLineSync());
+  // String output = (number > 0) ? 'Positif' : 'Negatif atau nol';
+  //
+  // // if(number > 0){
+  // //   output = 'Positif';
+  // // } else {
+  // //   output = 'Negatif atau nol';
+  // // }
+  //
+  // print(output);
+  switch (number) {
+    case 0:
+      print('nol');
+      break;
+    case 1:
+      print(number);
+      print('satu');
+      break;
+    case 2:
+      print('dua');
+      break;
+    default:
+      print('bilangan lain');
+  }
 }
